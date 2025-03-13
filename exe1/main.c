ea0a83ef888e6c8b68d4f6d9334c359d8df8d88f
Alison Araujo <alisonao@al.insper.edu.br>
2025-03-13 15:27:57 -0300

    1.3 2.0

--- exe1/main.c
+++ exe1/main.c
@@ -5,16 +5,16 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;  // Flag para detectar pressionamento do botão
+volatile int flag_f_r = 0;
 struct repeating_timer timer;
 
 bool timer_callback(struct repeating_timer *t) {
-    gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));  // Alterna o estado do LED
+    gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
     return true;
 }
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge (botão pressionado)
+    if (events == 0x4) {
         flag_f_r = 1;
     }
 }
@@ -22,34 +22,29 @@ void btn_callback(uint gpio, uint32_t events) {
 int main() {
     stdio_init_all();
 
-    // Inicializa o LED
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
-    gpio_put(LED_PIN_R, 0);  // Garante que o LED comece apagado
+    gpio_put(LED_PIN_R, 0);
 
-    // Inicializa o botão
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
-    // Configura a interrupção do botão
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-    bool timer_running = false;  // Variável local para controlar o estado do timer
+    bool timer_running = false;
 
     while (true) {
         if (flag_f_r) {
-            flag_f_r = 0;  // Reseta a flag
+            flag_f_r = 0;
 
             if (!timer_running) {
-                // Inicia o timer repetitivo
                 add_repeating_timer_ms(500, timer_callback, NULL, &timer);
                 timer_running = true;
             } else {
-                // Cancela o timer repetitivo
                 cancel_repeating_timer(&timer);
                 timer_running = false;
-                gpio_put(LED_PIN_R, 0);  // Garante que o LED termine apagado
+                gpio_put(LED_PIN_R, 0);
             }
         }
     }
--- exe2/main.c
+++ exe2/main.c
@@ -11,42 +11,78 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+struct repeating_timer timer_red;
+struct repeating_timer timer_green;
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
-        if (gpio == BTN_PIN_R)
+        if (gpio == BTN_PIN_R) {
             flag_r = 1;
-        else if (gpio == BTN_PIN_G)
+            else if (gpio == BTN_PIN_G)
             flag_g = 1;
-    }
+                }
+}
+
+bool timer_callback_red(struct repeating_timer *t) {
+    gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+    return true;
+}
+
+bool timer_callback_green(struct repeating_timer *t) {
+    gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
+    return true;
 }
 
 int main() {
+    stdio_init_all();
+    
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
-
+    gpio_put(LED_PIN_R, 0);
+    
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
-
+    gpio_put(LED_PIN_G, 0);
+    
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-
+    
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
+    
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+        
+    bool timer_running_red = false;
+    bool timer_running_green = false;
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
-    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
-
-    while (true) {
-
+    while (true) {  
         if (flag_r) {
             flag_r = 0;
+
+            if(!timer_running_red){
+                add_repeating_timer_ms(500, timer_callback_red, NULL, &timer_red);
+                timer_running_red = true;
+            } else {
+                cancel_repeating_timer(&timer_red);
+                timer_running_red = false;
+                gpio_put(LED_PIN_R, 0);
+            }
         }
 
         if (flag_g) {
             flag_g = 0;
+            
+            if(!timer_running_green){
+                add_repeating_timer_ms(250, timer_callback_green, NULL, &timer_green);
+                timer_running_green = true;
+            } else {
+                cancel_repeating_timer(&timer_green);
+                timer_running_green = false;
+                gpio_put(LED_PIN_G, 0);
+            }
         }
     }
 }
